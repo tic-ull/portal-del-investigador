f3c82d2cd0f12dc597a87e0a04c0c9e54b0c7be6
Elena Marrero-Mendez <emarrero@ull.edu.es>
2013-10-02 15:10:14 +0100

    Cambiadas las rutas a /investigacion/ porque todo colgará desde ahí

--- ViinV/urls.py
+++ ViinV/urls.py
@@ -8,21 +8,21 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ViinV.views.home', name='home'),
-    # url(r'^ViinV/', include('ViinV.foo.urls')),
+    # url(r'^investigacion/$', 'ViinV.views.home', name='home'),
+    # url(r'^investigacion/ViinV/', include('ViinV.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^investigacion/admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^investigacion/admin/', include(admin.site.urls)),
     
     # Página principal
-    url(r'^$', 'cvn.views.main', name='main'),
+    url(r'^investigacion/$', 'cvn.views.main', name='main'),
     
-    url(r'^cvn/$', 'cvn.views.index', name='index'),
+    url(r'^investigacion/cvn/$', 'cvn.views.index', name='index'),
     # Login/Logout CAS
-    url(r'^accounts/login/$', 'django_cas.views.login', name='login'), 
-    url(r'^accounts/logout/$', 'django_cas.views.logout', name='logout'),
+    url(r'^investigacion/accounts/login/$', 'django_cas.views.login', name='login'), 
+    url(r'^investigacion/accounts/logout/$', 'django_cas.views.logout', name='logout'),
 
 )
