4b4a2458a58c7cafe29cbaa084f1d5e787204c0c
Elena Marrero-Mendez <emarrero@ull.edu.es>
2013-11-19 09:25:29 +0000

    Reidentado el forms.py

--- cvn/forms.py
+++ cvn/forms.py
@@ -1,22 +1,15 @@
 # -*- coding: utf-8 -*-
-
-from django.forms import ModelForm
-from viinvDB.models import GrupoinvestInvestcvn
-
-# Funciones de apoyo
-from cvn.helpers import setCVNFileName
-
-from django.contrib.admin.widgets import AdminFileWidget
 from bootstrap_toolkit.widgets import BootstrapUneditableInput
-from django import forms
-
-#
-from django.forms.widgets import ClearableFileInput
-import os
-# missing imports
-from django.utils.safestring import mark_safe
 from cgi import escape
+from cvn.helpers import setCVNFileName
+from django import forms
+from django.contrib.admin.widgets import AdminFileWidget
+from django.forms import ModelForm
+from django.forms.widgets import ClearableFileInput
 from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
+from viinvDB.models import GrupoinvestInvestcvn
+import os
 
 class ShortNameClarableFileInput(ClearableFileInput):
     def render(self, name, value, attrs=None):
@@ -26,14 +19,14 @@ class ShortNameClarableFileInput(ClearableFileInput):
             'clear_template': '',
             'clear_checkbox_label': self.clear_checkbox_label,
         }
-        
+
         template = u'%(input)s'
-                
+
         substitutions['input'] = super(ClearableFileInput, self).render(name, value, attrs)
-        
+
         if value and hasattr(value, "url"):
-            template = self.template_with_initial            
-            
+            template = self.template_with_initial
+
             substitutions['initial'] = (u'<a href="%s" target>%s</a>'
                                         % (escape(value.url),
                                            escape(force_unicode('')))) # I just changed this line
@@ -43,28 +36,19 @@ class ShortNameClarableFileInput(ClearableFileInput):
                 substitutions['clear_checkbox_name'] = conditional_escape(checkbox_name)
                 substitutions['clear_checkbox_id'] = conditional_escape(checkbox_id)
                 substitutions['clear'] = CheckboxInput().render(checkbox_name, False, attrs={'id': checkbox_id})
-                substitutions['clear_template'] = self.template_with_clear % substitutions        
-        string = template % substitutions        
+                substitutions['clear_template'] = self.template_with_clear % substitutions
+        string = template % substitutions
         return mark_safe(string.replace(':','').replace('<br />',''))
         #return mark_safe(template % substitutions)
-        
+
 
 class UploadCvnForm(ModelForm):
-	""" 
-		Formulario que recoge un CVN en formato PDF e introduce sus datos en la BBDD de la aplicación CVN
-	"""
-	# TODO: Eliminar cuando en el modelo original se quite la restricción de 'blank=True' en dicho campo.
-	#~ cvnfile = forms.FileField()
-	
-	class Meta:
-		model = GrupoinvestInvestcvn		
-		widgets = {			
-			'cvnfile': ShortNameClarableFileInput,
-		}
-		fields = ('cvnfile',) #'investigador'	
-
-
-
-
-	
-
+    """
+        Formulario que recoge un CVN en formato PDF e introduce sus datos en la BBDD de la aplicación CVN
+    """
+    class Meta:
+        model = GrupoinvestInvestcvn
+        widgets = {
+            'cvnfile': ShortNameClarableFileInput,
+        }
+        fields = ('cvnfile',) #'investigador'
