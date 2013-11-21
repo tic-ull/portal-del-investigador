3631bda927b413871e4120cfb42abd3bf587ec6a
Luis Cerrudo <lcerrudo@ull.edu.es>
2013-11-21 13:34:43 +0000

    Añadido rutas de las fixtures nuevas

---
+++ ViinV/settings_tests.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+
+from django.conf import settings as st
+
+import os
+import time
+
+CHROME_DRIVER = "/home/luis/Descargas/chromedriver"
+
+ID_TEST      = 99999
+TEST_SERVER  = 'http://10.219.4.213:8000'
+
+CVN_LOCATION            = os.path.join(st.PROJECT_ROOT, 'cvn/tests/CVN-invbecario.pdf')
+CVN_NOT_FECYT_LOCATION  = os.path.join(st.PROJECT_ROOT, 'cvn/tests/CVN-invbecario-notFecyt.pdf')
+CVN_FILE     			= os.path.join(st.MEDIA_ROOT, 'cvn/pdf/CVN-test-709bddb1.pdf')
+XML_FILE     			= os.path.join(st.MEDIA_ROOT, 'cvn/xml/CVN-test-709bddb1.xml')
+
+USER_LOGIN   = "invbecario"
+USER_PASSWD  = "pruebasINV1"
+USER_NIF     = "123456789A"
+
+ADMIN_LOGIN  = "admin"
+ADMIN_PASSWD = "123"
+
+
+FIXTURE_DIRS = (
+    os.path.join(st.PROJECT_ROOT, 'cvn/tests/fixtures/'), 
+)
