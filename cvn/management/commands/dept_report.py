b33ff344631a49ebe58212cd5feb06f4f7c6a03a
dyeray <dyeray@ull.es>
2014-03-17 15:01:34 +0000

    Deleted class get_datos_departamento

--- cvn/management/commands/dept_report.py
+++ cvn/management/commands/dept_report.py
@@ -1,13 +1,19 @@
 # -*- encoding: UTF-8 -*-
 
-from cvn.models import Publicacion, Congreso, Proyecto, Convenio, TesisDoctoral
+from cvn.models import Publicacion, Congreso, Proyecto, Convenio, TesisDoctoral, Usuario
+from django.db.models import Q
 from django.core.management.base import BaseCommand, CommandError
-from get_datos_departamento import Get_datos_departamento
 from informe_pdf import Informe_pdf
 from optparse import make_option
-from viinvDB.models import GrupoinvestDepartamento
+from viinvDB.models import GrupoinvestDepartamento, GrupoinvestInvestigador
+import datetime
 
-
+def checkDigit(obj):
+    if obj is None or not obj.isdigit():
+        return False
+    else:
+        return True
+            
 class Command(BaseCommand):
     help = u'Genera un PDF con los datos de un Departamento'
     option_list = BaseCommand.option_list + (
@@ -29,16 +35,16 @@ class Command(BaseCommand):
         self.checkArgs(options)
         self.create_report()
 
-    def checkArgs(self, options):
-        if not options['year']:
-            raise CommandError("Option `--year=YYYY` must be specified.")
+    def checkArgs(self, options): 
+        if not checkDigit(options['year']):
+            raise CommandError("Option `--year=YYYY` must exist and be a number.")
         else:
-            self.year = options['year']
-
-        if not options['id']:
-            raise CommandError("Option `--id=X` must be specified.")
+            self.year = int(options['year'])
+        
+        if not checkDigit(options['id']):
+            raise CommandError("Option `--id=X` must exist and be a number.")
         else:
-            self.deptID = options['id']
+            self.deptID = int(options['id'])
 
     def create_report(self):
         (departamento, investigadores, articulos,
@@ -50,10 +56,11 @@ class Command(BaseCommand):
         informe.go()
 
     def getData(self):
-        dataDept = Get_datos_departamento(self.deptID, self.year)
-        usuarios = dataDept.get_usuarios()
+        #dataDept = Get_datos_departamento(self.deptID, self.year)
+        investigadores, usuarios = self.query_investigadores(self.deptID)
+        #usuarios = dataDept.get_usuarios()
         departamento = GrupoinvestDepartamento.objects.get(id=self.deptID)
-        investigadores = dataDept.get_investigadores()
+        #investigadores = dataDept.get_investigadores()
         articulos = Publicacion.objects.byUsuariosYearTipo(
             usuarios, self.year, 'Artículo'
         )
@@ -64,9 +71,29 @@ class Command(BaseCommand):
             usuarios, self.year, 'Capítulo de Libro'
         )
         congresos = Congreso.objects.byUsuariosYear(usuarios, self.year)
-        proyectos = Proyecto.objects.byUsuariosYear(usuarios, int(self.year))
-        convenios = Convenio.objects.byUsuariosYear(usuarios, int(self.year))
+        proyectos = Proyecto.objects.byUsuariosYear(usuarios, self.year)
+        convenios = Convenio.objects.byUsuariosYear(usuarios, self.year)
         tesis = TesisDoctoral.objects.byUsuariosYear(usuarios, self.year)
         return (departamento, investigadores, articulos,
                 libros, capitulosLibro, congresos, proyectos,
                 convenios, tesis)
+
+    def query_investigadores(self, identificador, tipo="departamento"):
+        assert (tipo == "departamento" or tipo == "instituto")
+        fecha_inicio_max = datetime.date(self.year, 12, 31)
+        fecha_fin_min = datetime.date(self.year, 1, 1)
+        investigadores = None
+        if tipo == "departamento":
+            investigadores = GrupoinvestInvestigador.objects.filter(departamento__id=identificador)
+        else:
+            investigadores = GrupoinvestInvestigador.objects.filter(instituto__id=identificador)
+        
+        investigadores = investigadores.filter(Q(fecha_inicio__isnull=False)&Q(fecha_inicio__lte=fecha_inicio_max))
+        investigadores = investigadores.filter(Q(cese__isnull=True)|Q(cese__gte=fecha_fin_min))
+        investigadores = investigadores.order_by('apellido1', 'apellido2')
+        
+        lista_dni = [investigador.nif for investigador in investigadores]
+        # Guardamos los objectos Usuario, de los investigadores GrupoinvestInvestigador
+        # Se extraen de esta manera por estar en bbdd diferentes
+        usuarios = Usuario.objects.filter(documento__in=lista_dni)
+        return investigadores, usuarios
