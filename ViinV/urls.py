63ddb42d47ca3d927361e0e791e6e4da0ebed025
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-03-21 12:57:51 +0000

    FIXED: Link to index from Templates for Gestionar CVN

--- ViinV/urls.py
+++ ViinV/urls.py
@@ -1,28 +1,18 @@
 # -*- encoding: utf-8 -*-
-from django.conf import settings
+
+from django.conf import settings as st
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^investigacion/$', 'ViinV.views.home', name='home'),
-    # url(r'^investigacion/ViinV/', include('ViinV.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^investigacion/admin/doc/',
-    #     include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^investigacion/admin/', include(admin.site.urls)),
 
     # Página principal
     url(r'^investigacion/$', 'cvn.views.main', name='main'),
-
     url(r'^investigacion/cvn/$', 'cvn.views.index', name='index'),
     url(r'^investigacion/cvn/ull_report/$', 'cvn.views.ull_report',
         name='ull_report'),
@@ -40,5 +30,5 @@ urlpatterns = patterns(
         'django_cas.views.logout',
         name='logout'),
 
-) + static(settings.MEDIA_URL,
-           document_root=settings.MEDIA_ROOT)  # Servir los pdfs
+) + static(st.MEDIA_URL,
+           document_root=st.MEDIA_ROOT)
