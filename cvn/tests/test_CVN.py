95e264e257fec97d9113b1e87cd8048536c8c1cd
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-04-08 13:24:23 +0100

    Test for insert XML to DDBB

---
+++ cvn/tests/test_CVN.py
@@ -0,0 +1,30 @@
+# -*- encoding: UTF-8 -*-
+
+from django.test import TestCase
+from cvn.models import CVN
+from django.contrib.auth.models import User
+from django.conf import settings as st
+import os
+
+
+class CVNTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(username='rabadmar')
+
+    def test_insertXML(self):
+        """ Insert XML data in BBDD """
+        try:
+            XML = os.path.join(st.MEDIA_ROOT, 'cvn/xml/CVN-rabadmar.xml')
+            fileXML = open(XML, 'r')
+            cvn = CVN(xml_file=fileXML)
+            cvn.insertXML(self.user.profile)
+            self.assertEqual(self.user.profile.publicacion_set.filter(tipo_de_produccion='Articulo').count(), 0)
+            self.assertEqual(self.user.profile.publicacion_set.filter(tipo_de_produccion='Libro').count(), 0)
+            self.assertEqual(self.user.profile.publicacion_set.filter(tipo_de_produccion='Capitulo de Libro').count(), 0)
+            self.assertEqual(self.user.profile.congreso_set.count(), 0)
+            self.assertEqual(self.user.profile.convenio_set.count(), 0)
+            self.assertEqual(self.user.profile.proyecto_set.count(), 1)
+            self.assertEqual(self.user.profile.tesisdoctoral_set.count(), 0)
+        except IOError:
+            pass
