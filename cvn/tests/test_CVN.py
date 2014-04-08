1c1d6ddc0785dd827175d7e282a85e29036db727
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-04-08 14:00:37 +0100

    Refactoring Test: insertXML

--- cvn/tests/test_CVN.py
+++ cvn/tests/test_CVN.py
@@ -13,18 +13,21 @@ class CVNTestCase(TestCase):
         self.user = User.objects.create(username='rabadmar')
 
     def test_insertXML(self):
-        """ Insert XML data in BBDD """
+        """ Insert the data of XML data in the database """
         try:
             XML = os.path.join(st.MEDIA_ROOT, 'cvn/xml/CVN-rabadmar.xml')
             fileXML = open(XML, 'r')
             cvn = CVN(xml_file=fileXML)
             cvn.insertXML(self.user.profile)
-            self.assertEqual(self.user.profile.publicacion_set.filter(tipo_de_produccion='Articulo').count(), 0)
-            self.assertEqual(self.user.profile.publicacion_set.filter(tipo_de_produccion='Libro').count(), 0)
-            self.assertEqual(self.user.profile.publicacion_set.filter(tipo_de_produccion='Capitulo de Libro').count(), 0)
+            self.assertEqual(self.user.profile.publicacion_set.filter(
+                tipo_de_produccion='Articulo').count(), 0)
+            self.assertEqual(self.user.profile.publicacion_set.filter(
+                tipo_de_produccion='Libro').count(), 0)
+            self.assertEqual(self.user.profile.publicacion_set.filter(
+                tipo_de_produccion='Capitulo de Libro').count(), 0)
             self.assertEqual(self.user.profile.congreso_set.count(), 0)
             self.assertEqual(self.user.profile.convenio_set.count(), 0)
             self.assertEqual(self.user.profile.proyecto_set.count(), 1)
             self.assertEqual(self.user.profile.tesisdoctoral_set.count(), 0)
-        except IOError:
-            pass
+        except:
+            raise
