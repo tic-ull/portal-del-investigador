30327c3df3e1e415fb6c15dd8e97375c2b489173
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-04-10 12:38:46 +0100

    Test for check data of Congress

--- cvn/tests/test_CVN.py
+++ cvn/tests/test_CVN.py
@@ -4,16 +4,21 @@ from cvn import settings as stCVN
 from cvn.models import CVN
 from django.test import TestCase
 from factories import UserFactory, AdminFactory
+from lxml import etree
 import os
 
 
 class CVNTestCase(TestCase):
 
     def setUp(self):
-        self.xml_ull = open(os.path.join(stCVN.TEST_ROOT, 'xml/CVN-ULL.xml'), 'r')
-        self.xml_empty =  open(os.path.join(stCVN.TEST_ROOT, 'xml/empty.xml'), 'r')
+        self.xml_ull = open(
+            os.path.join(stCVN.TEST_ROOT, 'xml/CVN-ULL.xml'), 'r')
+        self.xml_empty = open(
+            os.path.join(stCVN.TEST_ROOT, 'xml/empty.xml'), 'r')
+        self.xml_test = open(
+            os.path.join(stCVN.TEST_ROOT, 'xml/CVN-Test.xml'), 'r')
 
-    def test_insertXML_ULL(self):
+    def test_insert_xml_ull(self):
         """ Insert the data of XML in the database """
         try:
             cvn = CVN(xml_file=self.xml_ull)
@@ -31,6 +36,34 @@ class CVNTestCase(TestCase):
             self.assertEqual(user.profile.tesisdoctoral_set.count(), 0)
         except:
             raise
+
+    def test_check_insert_data_congreso(self):
+        cvn = CVN(xml_file=self.xml_test)
+        cvn.xml_file.seek(0)
+        items = etree.parse(cvn.xml_file).findall('CvnItem')
+        for item in items:
+            data = {}
+            key = item.find('CvnItemID/CVNPK/Item').text.strip()
+            if (key in stCVN.MODEL_TABLE and
+               stCVN.MODEL_TABLE[key] == 'Congreso'):
+                data = cvn._dataCongress(item)
+                self.assertIn(u'titulo', data)
+                self.assertEqual(data[u'titulo'], u'Título')
+                self.assertIn(u'nombre_del_congreso', data)
+                self.assertEqual(
+                    data[u'nombre_del_congreso'], u'Nombre del congreso')
+                self.assertIn(u'fecha_realizacion', data)
+                self.assertEqual(data[u'fecha_realizacion'], u'2014-04-01')
+                self.assertIn(u'fecha_finalizacion', data)
+                self.assertEqual(data[u'fecha_finalizacion'], u'2014-04-05')
+                self.assertIn(u'ciudad_de_realizacion', data)
+                self.assertEqual(
+                    data[u'ciudad_de_realizacion'], u'Ciudad de realización')
+                self.assertIn(u'autores', data)
+                self.assertEqual(data[u'autores'], u'STIC')
+                self.assertIn(u'ambito', data)
+                self.assertEqual(data[u'ambito'], u'Autonómica')
+                break
 
     def test_on_insert_cvn_old_production_tables_are_deleted(self):
         try:
