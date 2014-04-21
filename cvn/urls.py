4a708b8d40607eed90f09cdaf1ccd473e2b03dd3
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-04-21 14:07:10 +0100

    FIXED: CVN urls

--- cvn/urls.py
+++ cvn/urls.py
@@ -1,11 +1,10 @@
 # -*- encoding: UTF-8 -*-
 
-from cvn import views
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
-    '',
-    url(r'^$', views.index, name='index'),
-    url(r'^ull_report/$', views.ull_report, name='ull_report'),
-    url(r'^download/$', views.download_cvn, name='download_cvn'),
+    'cvn.views',
+    url(r'^$', 'index', name='index'),
+    url(r'^ull_report/$', 'ull_report', name='ull_report'),
+    url(r'^download/$', 'download_cvn', name='download_cvn'),
 )
