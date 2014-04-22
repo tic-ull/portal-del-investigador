fea5d3ff2b202d3130ab92721b3230c23c2dc725
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-04-22 11:00:21 +0100

    FIXED: length of name permission

--- cvn/signals.py
+++ cvn/signals.py
@@ -31,28 +31,28 @@ post_save.connect(create_profile, sender=User,
 
 
 # Related ticket http://code.djangoproject.com/ticket/4748
-def alter_django_auth_permissions(sender, **kwargs):
-    logger = logging.getLogger(__name__)
-    if not Permission in kwargs['created_models']:
-        return
-    SIZE_NAME = 128
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM auth_permission LIMIT 1")
-
-    for desc in cursor.description:
-        # See http://www.python.org/dev/peps/pep-0249/
-        (name, type_code, display_size, internal_size, precision,
-         scale, null_ok) = desc
-        if not name == 'name':
-            continue
-        if internal_size < SIZE_NAME:
-            logger.info('auth_permission: Column "name" gets altered.\
-                         Old: %d new: %d' % (internal_size, SIZE_NAME))
-            cursor.execute('''ALTER TABLE auth_permission ALTER COLUMN\
-                           "name" type VARCHAR(%s)''',
-                           [SIZE_NAME])
-        break
-    else:
-        raise Exception('table auth_permission has not column "name"')
-
-post_syncdb.connect(alter_django_auth_permissions)
+#def alter_django_auth_permissions(sender, **kwargs):
+#    logger = logging.getLogger(__name__)
+#    if not Permission in kwargs['created_models']:
+#        return
+#    SIZE_NAME = 128
+#    cursor = connection.cursor()
+#    cursor.execute("SELECT * FROM auth_permission LIMIT 1")
+#
+#    for desc in cursor.description:
+#        # See http://www.python.org/dev/peps/pep-0249/
+#        (name, type_code, display_size, internal_size, precision,
+#         scale, null_ok) = desc
+#        if not name == 'name':
+#            continue
+#        if internal_size < SIZE_NAME:
+#            logger.info('auth_permission: Column "name" gets altered.\
+#                         Old: %d new: %d' % (internal_size, SIZE_NAME))
+#            cursor.execute('''ALTER TABLE auth_permission ALTER COLUMN\
+#                           "name" type VARCHAR(%s)''',
+#                           [SIZE_NAME])
+#        break
+#    else:
+#        raise Exception('table auth_permission has not column "name"')
+#
+#post_syncdb.connect(alter_django_auth_permissions)
