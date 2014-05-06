1fa88c3dad6b1e115f58b06774445c719efd4847
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-05-06 10:12:03 +0100

    FIXED: get email from attributes of Django CAS

--- core/backends.py
+++ core/backends.py
@@ -1,39 +1,32 @@
 # -*- encoding: UTF-8 -*-
 
-import django_cas
-from django_cas.backends import _verify
 from django.contrib.auth.models import User
+from django_cas.backends import _verify
+import django_cas
 
 
 class CASBackend(django_cas.backends.CASBackend):
+
     def authenticate(self, ticket, service, request):
         """Verifies CAS ticket and gets or creates User object"""
-
         username, attributes = _verify(ticket, service)
-        if attributes and 'mail' in attributes:
-            # correos ULL (original y alias) nos quedamos con el principal
-            try:
-                attributes['real_email'] = filter(
-                    lambda x: x.startswith(username + "@"),
-                    attributes['mail']).pop()
-            except IndexError:
-                attributes['real_email'] = attributes['mail']
-            except AttributeError:
-                attributes['real_email'] = attributes['mail']
-        request.session['attributes'] = attributes or {}
         documento = None
-        if 'NumDocumento' in attributes:
+        if attributes and 'NumDocumento' in attributes:
+            request.session['attributes'] = attributes
             documento = attributes['NumDocumento']
         if not documento:
             return None
         try:
             user = User.objects.get(profile__documento=documento)
         except User.DoesNotExist:
-            # user will have an "unusable" password
+            # User will have an "unusable" password
             user = User.objects.create_user(username, '')
-            ## Add to User more datas
-            user.first_name = attributes.get('first_name', '')[:30]
-            user.last_name = attributes.get('last_name', '')[:30]
-            user.email = attributes.get('real_email', '')
+            # Add to User more info
+            if 'first_name' in attributes:
+                user.first_name = attributes['first_name']
+            if 'last_name' in attributes:
+                user.last_name = attributes['last_name']
+            if 'email' in attributes:
+                user.email = attributes['email']
             user.save()
         return user
