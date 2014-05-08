29915bce54204fc3f6306826b8ff85f27b852a08
dyeray <dyeray@ull.es>
2014-05-08 11:59:10 +0100

    Minor changes

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,5 +2,4 @@ from django.conf import settings
 
 
 def external_urls(request):
-    urls = {'old_portal': settings.OLD_PORTAL_URL}
-    return urls
+    return {'old_portal': settings.OLD_PORTAL_URL}
