e2b5748ebc5ba23f6495aafbeb37f6475c5ff4ab
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-05-12 13:09:16 +0100

    Added Admin Command for the migrate of Users and CVNs

---
+++ cvn/management/commands/migrate_cvn_from_csv.py
@@ -0,0 +1,38 @@
+# -*- encoding: UTF-8 -*-
+
+from core.models import UserProfile
+from cvn import settings as stCVN
+from cvn.forms import UploadCVNForm
+from django.conf import settings as st
+from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.management.base import BaseCommand
+import csv
+import os
+
+
+class Command(BaseCommand):
+    help = u'Migración de usuarios con su CVN desde un CSV del viejo portal'
+
+    def handle(self, *args,  **options):
+        import_path = os.path.join(st.BASE_DIR, 'importCVN/')
+        csv_file = import_path + 'users_to_migrate.csv'
+        with open(csv_file, 'rb') as csvfile:
+            lines = csv.reader(csvfile, delimiter=';')
+            for line in lines:
+                user = User.objects.get_or_create(
+                    username=unicode(line[0]), password='')[0]
+                UserProfile.objects.get_or_create(user=user)[0]
+                user.profile.documento = unicode(line[1])
+                user.profile.save()
+                upload_file = open(import_path + line[2], 'r')
+                cvn_file = SimpleUploadedFile(
+                    upload_file.name,
+                    upload_file.read(),
+                    content_type=stCVN.PDF)
+                form = UploadCVNForm(initial={'cvn_file': cvn_file}, user=user)
+                if form.is_valid():
+                    form.save()
+                else:
+                    print u'ERROR: CVN No válido (%s - %s)' % (
+                        line[0], line[2])
