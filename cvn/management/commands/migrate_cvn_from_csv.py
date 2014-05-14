524a036ad26616bb760d4e18c0da7dce7b868586
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-05-14 11:26:29 +0100

    Refactoring Migrate CVN

--- cvn/management/commands/migrate_cvn_from_csv.py
+++ cvn/management/commands/migrate_cvn_from_csv.py
@@ -20,9 +20,6 @@ class Command(BaseCommand):
         with open(csv_file, 'rb') as csvfile:
             lines = csv.reader(csvfile, delimiter=';')
             for line in lines:
-                print u'Línea: %s' % (lines.line_num)
-                print u'Usuario: %s' % (line[0])
-                print u'CVN: %s' % (line[2])
                 user = User.objects.get_or_create(
                     username=unicode(line[0]), password='')[0]
                 UserProfile.objects.get_or_create(user=user)[0]
@@ -31,9 +28,8 @@ class Command(BaseCommand):
                 try:
                     upload_file = open(import_path + line[2], 'r')
                 except IOError:
-                    print u'ERROR: CVN No encontrado (%s - %s)' % (
-                        line[0], line[2])
-                    print u'----------------------------------------'
+                    print u'[%s] \t ERROR: CVN No encontrado (%s - %s)' % (
+                        lines.line_num, line[0], line[2])
                     continue
                 cvn_file = SimpleUploadedFile(
                     upload_file.name,
@@ -42,8 +38,8 @@ class Command(BaseCommand):
                 form = UploadCVNForm(initial={'cvn_file': cvn_file}, user=user)
                 if form.is_valid():
                     form.save()
-                    print u'OK'
+                    print u'[%s] Usuario: %s - CVN: %s \t OK' % (
+                        lines.line_num, line[0], line[2])
                 else:
-                    print u'ERROR: CVN No válido (%s - %s)' % (
-                        line[0], line[2])
-                print u'----------------------------------------'
+                    print u'[%s] \t ERROR: CVN No válido (%s - %s)' % (
+                        lines.line_num, line[0], line[2])
