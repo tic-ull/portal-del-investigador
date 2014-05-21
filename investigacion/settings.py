7d1f4dca6fe3ba2c3ebbdcfabf728df22ecd065d
Luis Cerrudo <lcerrudo@ull.edu.es>
2014-05-21 15:00:58 +0100

    Merge with branch 'menu' (bootstrap v3)

--- investigacion/settings.py
+++ investigacion/settings.py
@@ -270,6 +270,15 @@ STATICFILES_FINDERS = (
 )
 
 # ---------------------------------------------------------- #
+TINYMCE_DEFAULT_CONFIG = {
+    'plugins': "table,spellchecker,paste,searchreplace",
+    'theme': "advanced",
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 10,
+}
+TINYMCE_SPELLCHECKER = True
+TINYMCE_COMPRESSOR = True
+# ---------------------------------------------------------- #
 
 try:
     from settings_local import *
