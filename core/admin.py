07de544e5452c410affd16c44a2178afe2678431
Luis Cerrudo <lcerrudo@ull.edu.es>
2014-05-26 12:05:01 +0100

    Add faq.html template in core Modified flatpage form in django template, isn't necessary specify the 'sites' and 'url' fields

--- core/admin.py
+++ core/admin.py
@@ -1,17 +1,17 @@
 # -*- encoding: UTF-8 -*-
 
+
+from core import settings as stCore
 from core.models import UserProfile, Log
+from django import forms
+from django.conf import settings as st
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-# Flatpages TinyMCE
 from django.contrib.flatpages.admin import FlatpageForm, FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage, Site
 from django.forms.widgets import HiddenInput, MultipleHiddenInput
-from django import forms
 from tinymce.widgets import TinyMCE
-from core import settings as stCore
-
 
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
@@ -47,36 +47,25 @@ class LogAdmin(admin.ModelAdmin):
         return tuple(Log._meta.get_all_field_names())
 
 
-
 class PageForm(FlatpageForm):
 
     url = forms.CharField(label='', max_length=100, required=False)
-#    enable_comments = forms.BooleanField(label='', required=False)
-#    template_name = forms.CharField(label='', max_length=70, required=False)
-#    registration_required = forms.BooleanField(label='', required=False)
     sites = forms.ModelMultipleChoiceField(queryset=Site.objects.all(),
                                            required=False, label='')
 
     def __init__(self, *args, **kwargs):
-        '''
-            Fields: url, title, content, sites, enable_comments,
-                    registration_required, template_name
-        '''
         super(FlatpageForm, self).__init__(*args, **kwargs)
-#        self.fields['url'].required = False
         self.fields['url'].initial = stCore.BASE_URL_FLATPAGES
         self.fields['url'].widget = HiddenInput()
-#        self.fields['sites'].required = False
         self.fields['sites'].widget = MultipleHiddenInput()
-#        self.fields['enable_comments'].widget = HiddenInput()
-#        self.fields['registration_required'].widget = HiddenInput()
-#        self.fields['template_name'].widget = HiddenInput()
+
+    def clean_sites(self):
+        return [Site.objects.get(id=st.SITE_ID)]
 
     def save(self, commit=True):
         flatpage = super(PageForm, self).save(commit=False)
         flatpage.save()
         flatpage.url = stCore.BASE_URL_FLATPAGES + str(flatpage.id) + '/'
-        flatpage.sites.add(Site.objects.all()[0])
         return flatpage
 
     class Meta:
@@ -88,9 +77,9 @@ class PageForm(FlatpageForm):
 
 class PageAdmin(FlatPageAdmin):
     form = PageForm
-    fieldsets = (
-        (None, {'fields': ('url', 'title', 'content', 'sites')}),
-    )
+#    fieldsets = (
+#        (None, {'fields': ('title', 'content')}),
+#    )
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
