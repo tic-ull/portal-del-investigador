b4bcacc4cbc636d3974af6f1673f624bc6851495
dyeray <dyeray@ull.es>
2014-05-26 09:16:52 +0100

    Translations per application

--- investigacion/settings.py
+++ investigacion/settings.py
@@ -31,7 +31,8 @@ USE_I18N = True
 USE_L10N = True
 
 LOCALE_PATHS = (
-    os.path.join(BASE_DIR, 'locale'),
+    os.path.join(BASE_DIR, 'core/locale'),
+    os.path.join(BASE_DIR, 'cvn/locale'),
 )
 
 MANAGERS = ADMINS
