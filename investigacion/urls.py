14d98bb9516a0382d1edb6a9af6d7f58c407342e
Luis Cerrudo <lcerrudo@ull.edu.es>
2014-05-28 12:23:10 +0100

    Add DetailView in 'investigacion/urls.py' for display flatpages individually Template 'question_faq.html' for display flatpages individually

--- investigacion/urls.py
+++ investigacion/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView, RedirectView
+from django.views.generic.detail import DetailView
+from django.contrib.flatpages.models import FlatPage
+
 
 admin.autodiscover()
 
@@ -20,8 +23,9 @@ urlpatterns = patterns(
         'django_cas.views.logout', name='logout'),
     url(r'^investigacion/faq/$', TemplateView.as_view(
         template_name='core/faq.html'), name='faq'),
-    url(r'^investigacion/faq/(?P<id>\d+)/$', TemplateView.as_view(
-        template_name='core/question_faq.html'), name='question_faq'),
+    url(r'^investigacion/faq/(?P<pk>\d+)/$', DetailView.as_view(
+        template_name='core/question_faq.html', model=FlatPage),
+        name='question_faq'),
     url(r'^investigacion/tinymce/', include('tinymce.urls')),
 )
 
