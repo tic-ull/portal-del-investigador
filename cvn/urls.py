031eb0a4215bf9c6ab532e62a1d6cee884f4cec4
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-06-26 09:09:13 +0100

    FIXED: URL Waiting of CVN

--- cvn/urls.py
+++ cvn/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns(
     url(r'^$', 'index', name='cvn'),
     url(r'^ull_report/$', 'ull_report', name='ull_report'),
     url(r'^download/$', 'download_cvn', name='download_cvn'),
-    url(r'^waiting$', lambda r: HttpResponse(status=204), name='waiting'),
+    url(r'^waiting/$', lambda r: HttpResponse(status=204), name='waiting'),
 )
