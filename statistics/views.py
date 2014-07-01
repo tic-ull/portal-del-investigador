1cb750602e5dec734b7defcee9ff2592ad5d3125
Luis Cerrudo <lcerrudo@ull.edu.es>
2014-07-01 14:31:53 +0100

    Fixed import error in 'statistics/views.py'

--- statistics/views.py
+++ statistics/views.py
@@ -1,7 +1,7 @@
 # -*- encoding: UTF-8 -*-
 
 from core.models import UserProfile
-from core.helpers import wsget
+from core.utils import wsget
 from cvn import settings as stCVN
 from django.conf import settings as st
 from django.contrib.admin.views.decorators import staff_member_required
