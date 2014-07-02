16448cf44138d57013be35001849a75a3c75795f
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-07-02 10:51:28 +0100

    Refactoring index of CVN

--- cvn/views.py
+++ cvn/views.py
@@ -1,22 +1,19 @@
 # -*- encoding: UTF-8 -*-
 
+from cvn import settings as stCVN
 from cvn.forms import UploadCVNForm
 from cvn.models import CVN
 from cvn.utils import scientific_production_to_context, cvn_to_context
-from django.conf import settings as st
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core import serializers
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.translation import ugettext as _
 from statistics import settings as stSt
-from statistics.models import Department
-from cvn import settings as stCVN
-import json
 import logging
-import urllib
 
 logger = logging.getLogger(__name__)
 
@@ -25,35 +22,30 @@ logger = logging.getLogger(__name__)
 def index(request):
     context = {}
     user = request.user
+    if 'dept' and 'dept_json' in request.session:
+        for obj in serializers.deserialize(
+                "json", request.session['dept'], ignorenonexistent=True):
+            dept = obj.object
+        dept_json = request.session['dept_json']
+        context['department'] = dept
+        context['validPercentCVN'] = stSt.PERCENT_VALID_DEPT_CVN
     form = UploadCVNForm()
-
-    # Get department code of the user from webservice, and with it,
-    # find the department statistics on database
-    try:
-        dept_json = json.loads(
-            urllib.urlopen(st.WS_INFO_USER % user.profile.rrhh_code).read())
-        dept = Department.objects.get(
-            code=dept_json['departamento']['cod_departamento'])
-    except (IOError, KeyError, ObjectDoesNotExist):
-        dept = None
-
     if request.method == 'POST':
         form = UploadCVNForm(request.POST, request.FILES, user=user)
         if form.is_valid():
             try:
-                old_status = CVN.objects.get(user_profile__user=user).status
+                old_cvn_status = CVN.objects.get(
+                    user_profile__user=user).status
             except ObjectDoesNotExist:
-                old_status = None
-            new_status = form.save().status
-            if old_status != new_status and dept is not None:
+                old_cvn_status = None
+            cvn = form.save()
+            context['message'] = _(u'CVN actualizado con éxito.')
+            if dept is not None and old_cvn_status != cvn.status:
                 dept.update(dept_json['departamento']['nombre'],
                             dept_json['miembros'], True)
-            context['message'] = _(u'CVN actualizado con éxito.')
     context['form'] = form
     cvn_to_context(user.profile, context)
     context['CVN'] = scientific_production_to_context(user.profile, context)
-    context['department'] = dept
-    context['validPercentCVN'] = stSt.PERCENT_VALID_DEPT_CVN
     context['TIME_WAITING'] = stCVN.TIME_WAITING
     context['MESSAGES_WAITING'] = stCVN.MESSAGES_WAITING
     return render(request, 'cvn/index.html', context)
