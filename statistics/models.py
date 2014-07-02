105b287b373e759b9eca9896e1054f7fefaf63bd
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-07-02 10:50:03 +0100

    Get department from signal post login

--- statistics/models.py
+++ statistics/models.py
@@ -2,10 +2,13 @@
 
 from core.models import UserProfile
 from cvn import settings as stCVN
+from django.conf import settings as st
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from statistics.managers import StatsManager, ProfessionalCategoryManager
+import json
+import urllib
 
 
 class Stats(models.Model):
@@ -52,7 +55,17 @@ class Stats(models.Model):
 
 
 class Department(Stats):
-    pass
+
+    @staticmethod
+    def get_user_department(rrhh_code):
+        try:
+            dept_json = json.loads(
+                urllib.urlopen(st.WS_INFO_USER % rrhh_code).read())
+            dept = Department.objects.get(
+                code=dept_json['departamento']['cod_departamento'])
+        except (IOError, KeyError, ObjectDoesNotExist):
+            return None, None
+        return dept, dept_json
 
 
 class Area(Stats):
