14d79abcf6627059bcaf5f5b84f91d5aa8a5b2fc
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-07-16 14:34:38 +0100

    FIXED: Eval the output of get_redis

--- core/utils.py
+++ core/utils.py
@@ -9,23 +9,36 @@ POOL = redis.ConnectionPool(host=st.REDIS_HOST, port=st.REDIS_PORT,
                             db=st.REDIS_DB, password=st.REDIS_PASSWORD)
 
 
-def get_redis(key):
-    r = redis.Redis(connection_pool=POOL)
-    return r.get(key)
-
-
-def set_redis(key, value):
-    r = redis.Redis(connection_pool=POOL)
-    r.set(key, value)
-
-
 def wsget(ws):
     try:
         ws_json = json.loads(urllib.urlopen(ws).read())
         if (type(ws_json) is dict and
                 'faultcode' in ws_json.keys()):
-            return get_redis(ws)
+            return eval_get_redis(ws)
         set_redis(ws, ws_json)
         return ws_json
     except:
-        return get_redis(ws)
+        return eval_get_redis(ws)
+
+
+def eval_get_redis(ws):
+    value = get_redis(ws)
+    if value is not None:
+        return eval(value.decode('string-escape'))
+    return None
+
+
+def get_redis(key):
+    try:
+        r = redis.Redis(connection_pool=POOL)
+        return r.get(key)
+    except redis.ConnectionError:
+        return None
+
+
+def set_redis(key, value):
+    try:
+        r = redis.Redis(connection_pool=POOL)
+        r.set(key, value)
+    except redis.ConnectionError:
+        pass
