5ab6a9ecf96db37c97dfab9986cb28f79ac75566
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-07-21 14:37:59 +0100

    Use REDIS is optional

--- core/redis_utils.py
+++ core/redis_utils.py
@@ -9,16 +9,23 @@ POOL = redis.ConnectionPool(host=st.REDIS_HOST, port=st.REDIS_PORT,
                             db=st.REDIS_DB, password=st.REDIS_PASSWORD)
 
 
-def wsget(ws):
+def wsget(ws, use_redis=True, timeout=st.REDIS_TIMEOUT):
     try:
         ws_json = json.loads(urllib.urlopen(ws).read())
         if (type(ws_json) is dict and
                 'faultcode' in ws_json.keys()):
-            return eval_get_redis(ws)
-        set_redis(ws, ws_json)
+            return get_from_redis_or_none(ws, use_redis)
+        if use_redis:
+            set_redis(ws, timeout, ws_json)
         return ws_json
     except:
+        return get_from_redis_or_none(ws, use_redis)
+
+
+def get_from_redis_or_none(ws, use_redis):
+    if use_redis:
         return eval_get_redis(ws)
+    return None
 
 
 def eval_get_redis(ws):
@@ -36,9 +43,11 @@ def get_redis(key):
         return None
 
 
-def set_redis(key, value):
+def set_redis(key, timeout, value):
     try:
         r = redis.Redis(connection_pool=POOL)
-        r.setex(key, st.REDIS_TIMEOUT, value)
+        if timeout is None or not str(timeout).isdigit():
+            r.set(key, value)
+        r.setex(key, timeout, value)
     except redis.ConnectionError:
         pass
