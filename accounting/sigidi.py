ff136afdaf21702d47d43c707e6c167c34339a37
dyeray <dyeray@ull.es>
2014-09-03 13:20:44 +0100

    Continue implementation of sigidi permissions connection. Fix bug with big surnames

--- accounting/sigidi.py
+++ accounting/sigidi.py
@@ -1,8 +1,14 @@
 from django.db import connections
+from django.contrib.auth.models import User
+import re
 
-class SigidiConnection:
+class SigidiPermissions:
+    CONTAB_RES = "ALLOW_CONTAB_RES"
+    CONTAB_LIST = "ALLOW_CONTAB_LIST"
 
-    dataid = 'select "DATAID" from "OBJ_2275" where "DNI"=\'PASMEN2\''
+class SigidiUserPermissions:
+
+    dataid = 'select "DATAID" from "OBJ_2275" where "DNI"=\'{0}\''
 
     proid = 'select "PROID" from "NIV_PRODUCTS" where "PRODATAID"' \
             ' in (' + dataid + ')'
@@ -10,11 +16,30 @@ class SigidiConnection:
     vrid = 'select "VRID" from "NIV_VALUES_REF" where "VRTARGETID"' \
            ' in (' + proid + ')'
 
-    codigo = 'select "CODIGO" from "OBJ_2216" where "ALLOW_CONTAB_RES"' \
-             ' in (' + vrid + ')'
+    permission_query = 'select "CODIGO" from "OBJ_2216" where "{0}"' \
+                       ' in ({1})'
 
-    def __init__(self):
+    def __init__(self, user):
         self.cursor = connections['sigidi'].cursor()
+        user = User.objects.get(profile__documento='PASMEN2')
+        self.cursor.execute(self.vrid.format(user.profile.documento))
+        user_permissions = self.cursor.fetchall()
+        user_permissions = re.sub('[\[\]()]|,,', '',
+                                       str(user_permissions)).strip(',')
+        self.user_permissions = re.sub(',,', ',', user_permissions)
 
-    def get_users(self):
-        pass
+    def has_perm(self, permission, project):
+        self.cursor.execute(self.permission_query.format(permission,
+                                                         self.user_permissions))
+        projects = self.cursor.fetchall()
+        for proj in projects:
+            if proj[0] == project:
+                return True
+        return False
+
+
+    def can_contab_res(self, project):
+        return self.has_perm(SigidiPermissions.CONTAB_RES, project)
+
+    def can_contab_list(self, project):
+        return self.has_perm(SigidiPermissions.CONTAB_LIST)
