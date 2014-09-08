253e9659da0ea4fccad3f9ef7fe33660c4e13622
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-09-08 14:00:43 +0100

    Added URL for show accounting detail

--- accounting/views.py
+++ accounting/views.py
@@ -9,3 +9,7 @@ def index(request):
     context = {}
     return render(request, 'accounting/base.html', context)
 
+@login_required
+def accounting_detail(request, code):
+    context = {}
+    return render(request, 'accounting/base.html', context)
