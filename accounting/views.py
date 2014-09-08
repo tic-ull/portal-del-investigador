ef3e168b0c5c9e7267d63f5e7f717952d0fc0bbb
Luis Cerrudo <lcerrudo@ull.edu.es>
2014-09-08 13:26:25 +0100

    Only show projects list with CONT_KEY Add columns CODIGO & CONT_KEY (only for staff user) Applied corporate visual style in the project list template

--- accounting/views.py
+++ accounting/views.py
@@ -9,6 +9,9 @@ from sigidi import SigidiConnection
 @login_required
 def index(request):
     context = dict()
-    context['list_projects'] = SigidiConnection(request.user).get_projects()
-    print context['list_projects']
+    context['list_projects'] = []
+    list_projects = SigidiConnection(request.user).get_projects()
+    for project in list_projects:
+        if project['CONT_KEY'] is not None:
+            context['list_projects'].append(project)
     return render(request, 'accounting/index.html', context)
