2b22472fc61ed7dfa05fcb209f06353bca7ac146
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-09-12 13:14:24 +0100

    Added more info about accounting

--- accounting/tables.py
+++ accounting/tables.py
@@ -8,12 +8,10 @@ import django_tables2 as tables
 class SummaryYearTable(Table):
 
     ejercicio = tables.Column(accessor='ejercicio',
-                              verbose_name=_(u'Ejercicio'),
-                              attrs={'th': {'width': '12%'}})
+                              verbose_name=_(u'Ejercicio'))
 
     inicial = tables.Column(accessor='inicial', verbose_name=_(u'Inicial'),
-                            orderable=False,
-                            attrs={'th': {'width': '8%'}})
+                            orderable=False)
 
     reserva = tables.Column(accessor='reserva',
                             verbose_name=_(u'Reserva de Crédito'),
@@ -21,16 +19,13 @@ class SummaryYearTable(Table):
 
     orgSaldoRC = tables.Column(accessor='orgSaldoRC',
                                verbose_name=_(u'Saldo RC'),
-                               orderable=False,
-                               attrs={'th': {'width': '10%'}})
+                               orderable=False)
 
     orgSaldoA = tables.Column(accessor='orgSaldoA', verbose_name=_(u'Saldo A'),
-                              orderable=False,
-                              attrs={'th': {'width': '10%'}})
+                              orderable=False)
 
     orgSaldoD = tables.Column(accessor='orgSaldoD', verbose_name=_(u'Saldo D'),
-                              orderable=False,
-                              attrs={'th': {'width': '10%'}})
+                              orderable=False)
 
     orgModificaciones = tables.Column(accessor='orgModificaciones',
                                       verbose_name=_(u'Modificaciones'),
@@ -54,13 +49,11 @@ class SummaryConceptTable(Table):
 
     funSaldoEXP = tables.Column(accessor='funSaldoEXP',
                                 verbose_name=_(u'Saldo EXP'),
-                                orderable=False,
-                                attrs={'th': {'width': '10%'}})
+                                orderable=False)
 
     funSaldoFAC = tables.Column(accessor='funSaldoFAC',
                                 verbose_name=_(u'Saldo Facturas'),
-                                orderable=False,
-                                attrs={'th': {'width': '10%'}})
+                                orderable=False)
 
     funSaldoA = tables.Column(accessor='funSaldoA',
                               verbose_name=_(u'Saldo A'),
@@ -72,47 +65,66 @@ class SummaryConceptTable(Table):
 
     funTotalObligaciones = tables.Column(accessor='funTotalObligaciones',
                                          verbose_name=_(u'Total Obligaciones'),
-                                         orderable=False,
-                                         attrs={'th': {'width': '10%'}})
+                                         orderable=False)
 
     class Meta:
         attrs = {'class': 'table table-striped table-condensed'}
 
 
-class BreakdownYearTable(Table):
+class BreakdownYearTable(SummaryConceptTable):
 
     ejercicio = tables.Column(accessor='ejercicio',
-                              verbose_name=_(u'Ejercicio'),
-                              attrs={'th': {'width': '12%'}})
+                              verbose_name=_(u'Ejercicio'))
 
     concepto = tables.Column(accessor='concepto',
                              verbose_name=_(u'Concepto'),
                              orderable=False)
 
-    funSaldoEXP = tables.Column(accessor='funSaldoEXP',
-                                verbose_name=_(u'Saldo EXP'),
-                                orderable=False,
-                                attrs={'th': {'width': '10%'}})
+    class Meta:
+        attrs = {'class': 'table table-striped table-condensed'}
+        fields = ('ejercicio', )
 
-    funSaldoFAC = tables.Column(accessor='funSaldoFAC',
-                                verbose_name=_(u'Saldo Facturas'),
-                                orderable=False,
-                                attrs={'th': {'width': '10%'}})
 
-    funSaldoA = tables.Column(accessor='funSaldoA',
-                              verbose_name=_(u'Saldo A'),
-                              orderable=False,
+class DetailTable(Table):
+
+    ejercicio = tables.Column(accessor='ejercicio',
+                              verbose_name=_(u'Ejercicio'),
                               attrs={'th': {'width': '10%'}})
 
-    funSaldoD = tables.Column(accessor='funSaldoD',
-                              verbose_name=_(u'Saldo D'),
-                              orderable=False,
-                              attrs={'th': {'width': '10%'}})
+    numero = tables.Column(accessor='numero',
+                           verbose_name=_(u'Número'))
 
-    funTotalObligaciones = tables.Column(accessor='funTotalObligaciones',
-                                         verbose_name=_(u'Total Obligaciones'),
-                                         orderable=False,
-                                         attrs={'th': {'width': '10%'}})
+    tipo = tables.Column(accessor='tipo', verbose_name=_(u'Tipo'),
+                         orderable=False)
+
+    proveedor = tables.Column(accessor='proveedor',
+                              verbose_name=_(u'Proveedor'),
+                              orderable=False)
+
+    descripcion = tables.Column(accessor='descripcion',
+                                verbose_name=_(u'Descripción'),
+                                orderable=False)
+
+    concepto = tables.Column(accessor='concepto',
+                             verbose_name=_(u'Concepto'),
+                             orderable=False)
+
+    variaciones = tables.Column(accessor='variaciones',
+                                verbose_name=_(u'Variaciones'),
+                                orderable=False)
+
+    gastos = tables.Column(accessor='gastos', verbose_name=_(u'Gastos'),
+                           orderable=False)
+
+    asiento = tables.Column(accessor='asiento', verbose_name=_(u'Asiento'),
+                            orderable=False)
+
+    validacion = tables.Column(accessor='validacion',
+                               verbose_name=_(u'Validación'),
+                               orderable=False)
+
+    pago = tables.Column(accessor='pago', verbose_name=_(u'Pago'),
+                         orderable=False)
 
     class Meta:
         attrs = {'class': 'table table-striped table-condensed'}
--- accounting/views.py
+++ accounting/views.py
@@ -3,10 +3,12 @@
 from core.ws_utils import CachedWS as ws
 from django.conf import settings as st
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import render
 from django_tables2 import RequestConfig
 from sigidi import SigidiConnection
-from tables import SummaryYearTable, SummaryConceptTable, BreakdownYearTable
+from tables import (SummaryYearTable, SummaryConceptTable, BreakdownYearTable,
+                    DetailTable)
 
 
 @login_required
@@ -25,29 +27,45 @@ def accounting_detail(request, code):
     context = dict()
     context['code'] = code
     project = SigidiConnection(user=request.user).get_project(code)
+
+    if not project:
+        raise Http404
+
     if 'CONT_KEY' in project and project['CONT_KEY'] is not None:
         accounting_code = project['CONT_KEY']
+
         if 'NAME' in project and project['NAME'] is not None:
             context['name'] = project['NAME']
+
         if ('ALLOW_CONTAB_RES' in project and
                 project['ALLOW_CONTAB_RES'] is not None):
 
             summary_year = ws.get(st.WS_RESUMEN_YEAR % accounting_code)
-            summary_year = SummaryYearTable(summary_year)
-            RequestConfig(request, paginate=False).configure(summary_year)
-            context['summary_year'] = summary_year
+            if len(summary_year):
+                summary_year = SummaryYearTable(summary_year)
+                RequestConfig(request, paginate=False).configure(summary_year)
+                context['summary_year'] = summary_year
 
             summary_concept = ws.get(st.WS_RESUMEN_CONCEPTO % accounting_code)
-            summary_concept = SummaryConceptTable(summary_concept)
-            RequestConfig(request, paginate=False).configure(summary_concept)
-            context['summary_concept'] = summary_concept
+            if len(summary_concept):
+                summary_concept = SummaryConceptTable(summary_concept)
+                RequestConfig(
+                    request, paginate=False).configure(summary_concept)
+                context['summary_concept'] = summary_concept
 
             breakdown_year = ws.get(st.WS_DESGLOSE_YEAR % accounting_code)
-            breakdown_year = BreakdownYearTable(breakdown_year)
-            RequestConfig(request, paginate=False).configure(breakdown_year)
-            context['breakdown_year'] = breakdown_year
+            if len(breakdown_year):
+                breakdown_year = BreakdownYearTable(breakdown_year)
+                RequestConfig(request, paginate=False).configure(breakdown_year)
+                context['breakdown_year'] = breakdown_year
 
-        # if ('ALLOW_CONTAB_LIST' in project and
-        #        project['ALLOW_CONTAB_LIST'] is not None):
-        #    context['detalles'] = ws.get(st.WS_DETALLES % accounting_code)
+        if ('ALLOW_CONTAB_LIST' in project and
+                project['ALLOW_CONTAB_LIST'] is not None):
+
+            detail = ws.get(st.WS_DETALLES % accounting_code)
+            if len(detail):
+                detail = DetailTable(detail)
+                RequestConfig(request, paginate=False).configure(detail)
+                context['detail'] = detail
+
     return render(request, 'accounting/accounting_detail.html', context)
