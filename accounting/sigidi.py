f5d7e9e5dbf4679da1e2f5d2118c63eb1c6944df
dyeray <dyeray@ull.es>
2014-10-03 11:13:07 +0100

    Fixed warnings and refactor sigidi.py

--- accounting/sigidi.py
+++ accounting/sigidi.py
@@ -30,69 +30,65 @@ class SigidiEntityType(IntEnum):
     CONVENIOS = 1
 
 
-sigidi_tables = {SigidiEntityType.PROYECTOS: 'OBJ_2216',
-                 SigidiEntityType.CONVENIOS: 'OBJ_2215'}
+class Sqry:
+    '''Here we store the Sigidi queries'''
 
-
-sigidi_dates = {SigidiEntityType.PROYECTOS: 'PROP_CONC_FECHA_ACEPT',
-                SigidiEntityType.CONVENIOS: 'FECHA_INICIO'}
-
-
-class SigidiConnection:
-
-    dataid = 'select "DATAID" from "OBJ_2275" where "DNI"=\'{0}\'' \
+    # vrid_query gets the ids of the user permissions. The returned values can
+    # be on any permission rows (SigidiPermissions)
+    _dataid = 'select "DATAID" from "OBJ_2275" where "DNI"=\'{0}\'' \
              ' and "ISACTIVE"=1'
+    _proid = 'select "PROID" from "NIV_PRODUCTS" where "PRODATAID"' \
+            ' in (' + _dataid + ')'
+    vrid_query = 'select "VRID" from "NIV_VALUES_REF" where "VRTARGETID"' \
+           ' in (' + _proid + ')'
 
-    proid = 'select "PROID" from "NIV_PRODUCTS" where "PRODATAID"' \
-            ' in (' + dataid + ')'
-
-    vrid = 'select "VRID" from "NIV_VALUES_REF" where "VRTARGETID"' \
-           ' in (' + proid + ')'
-
-    # Proyectos/Convenios for user with explicit permissions
+    # permission_query Returns Proyectos/Convenios for a user with explicit
+    # permissions set (SigidiPermissions)
+    permission_query_end = '"{0}" in ({1})'
     permission_query = 'select "CODIGO", "CONT_KEY", "ALLOW_CONTAB_RES", ' \
                        '"ALLOW_CONTAB_LIST", "NAME", "%(fecha_inicio)s",' \
                        ' "IP_ULL" from "%(table)s" where '
 
-    # All Proyectos/Convenios
+    # all_entities_query gets all Convenios/Proyectos
     all_entities_query = 'select "CODIGO", "CONT_KEY", 1 "ALLOW_CONTAB_RES",' \
                          ' 1 "ALLOW_CONTAB_LIST",' \
                          ' "NAME", "%(fecha_inicio)s", "IP_ULL"' \
                          ' from "%(table)s" order by "NAME"'
 
-    # One Proyecto/Convenio
+    # one_entity_query gets one Convenio/Proyecto
     one_entity_query = 'select "CODIGO", "CONT_KEY", "IP_ULL",' \
                        ' "NAME", "%(fecha_inicio)s", 1 "ALLOW_CONTAB_RES",' \
                        ' 1 "ALLOW_CONTAB_LIST" from "%(table)s"' \
                        'where "CODIGO"=\'%(entity)s\''
 
-    permission_query_end = '"{0}" in ({1})'
-
-    dataids_from_username = 'select "DATAID" from "OBJ_2274"' \
+    # user_categories_query gets the categories a user has (SigidiCategories).
+    # These categories define what type of gestor the user is
+    _dataids_from_username = 'select "DATAID" from "OBJ_2274"' \
                             'where "USERNAME"=\'{0}\' and "ISACTIVE"=1 and' \
                             '(("VALID_FROM" is NULL or "VALID_FROM" < NOW())' \
                             'and ("VALID_TO" is NULL or "VALID_TO" > NOW()))'
-
-    product_from_dataids = 'select "PROID" from "NIV_PRODUCTS"' \
+    _product_from_dataids = 'select "PROID" from "NIV_PRODUCTS"' \
                            ' where "PRODATAID"' \
-                           ' in (' + dataids_from_username + ')'
-
+                           ' in (' + _dataids_from_username + ')'
     user_categories_query = 'select "REL_PC_CATEGORYID" from "NIV_PROCATREL"' \
                             ' where "REL_PC_PRODUCTID"' \
-                            ' in (' + product_from_dataids + ')'
+                            ' in (' + _product_from_dataids + ')'
 
-    refs_from_dataids = 'select "VRID" from "NIV_VALUES_REF"' \
+    # entities_where_is_ip_query gets Proyectos/Convenios where the user is ip
+    _refs_from_dataids = 'select "VRID" from "NIV_VALUES_REF"' \
                         ' where "VRTARGETDATAID"' \
-                        ' in (' + dataid + ')'
-
-    # Proyectos/Convenios where is ip
+                        ' in (' + _dataid + ')'
     entities_where_is_ip_query = 'select "CODIGO", "CONT_KEY", "NAME",' \
                                  ' "IP_ULL", "%(fecha_inicio)s",' \
                                  ' 1 "ALLOW_CONTAB_RES",' \
                                  ' 1 "ALLOW_CONTAB_LIST" ' \
                                  'from "%(table)s" where "IP_ULL"' \
-                                 ' in (' + refs_from_dataids + ')'
+                                 ' in (' + _refs_from_dataids + ')'
 
+    # The macroquery can be combined with any query that returns
+    # Proyectos/Convenios (entities_where_is_ip_query, one_entity_query,
+    # all_entities_query, permission_query)
+    # It appends to the Proyectos/Convenios, the name of their IP.
     macroquery = 'select entities."CODIGO", entities."CONT_KEY",' \
                  ' "OBJ_2275"."NAME" "IP",' \
                  ' entities."NAME", entities."%(fecha_inicio)s" "DATE",' \
@@ -102,22 +98,30 @@ class SigidiConnection:
                  ' right join (%(entities_query)s) entities' \
                  ' on "NIV_VALUES_REF"."VRID" = entities."IP_ULL"' \
 
-    proyectos_permissions = set([
+
+class SigidiConnection:
+
+    _ent_tables = {SigidiEntityType.PROYECTOS: 'OBJ_2216',
+                   SigidiEntityType.CONVENIOS: 'OBJ_2215'}
+
+    _ent_dates = {SigidiEntityType.PROYECTOS: 'PROP_CONC_FECHA_ACEPT',
+                  SigidiEntityType.CONVENIOS: 'FECHA_INICIO'}
+
+    _proyectos_permissions = {
         SigidiCategories.SUPERUSUARIO,
         SigidiCategories.GESTOR_PROYECTOS,
-        SigidiCategories.GESTOR_PROYECTOS_SOLO_LECTURA])
+        SigidiCategories.GESTOR_PROYECTOS_SOLO_LECTURA}
 
-    convenios_permissions = set([
+    _convenios_permissions = {
         SigidiCategories.SUPERUSUARIO,
         SigidiCategories.GESTOR_CONVENIOS_Y_CONTRATOS,
-        SigidiCategories.GESTOR_CONVENIOS_Y_CONTRATOS_SOLO_LECTURA
-    ])
+        SigidiCategories.GESTOR_CONVENIOS_Y_CONTRATOS_SOLO_LECTURA}
 
     def __init__(self, user):
         self.user = user
         st.DATABASES['sigidi'] = st.SIGIDI_DB
         self.cursor = connections['sigidi'].cursor()
-        self.cursor.execute(self.vrid.format(user.profile.documento))
+        self.cursor.execute(Sqry.vrid_query.format(user.profile.documento))
         user_permissions = self.cursor.fetchall()
         user_permissions = re.sub('[\[\]()]|,,', '',
                                   str(user_permissions)).strip(',')
@@ -142,14 +146,14 @@ class SigidiConnection:
 
     def _query_entity(self, permissions, entity_type):
         """Builds dynamically query to get projects for a user"""
-        sentence = self.permission_query
+        sentence = Sqry.permission_query
         for permission in permissions:
-            sentence += self.permission_query_end.format(
+            sentence += Sqry.permission_query_end.format(
                 permission.value, self.user_permissions) + " OR "
         sentence = sentence[:-4]  # Remove the last OR
-        subquery = sentence % {'table': sigidi_tables[entity_type],
-                               'fecha_inicio': sigidi_dates[entity_type]}
-        return self._do_the_macroquery(subquery, sigidi_dates[entity_type])
+        subquery = sentence % {'table': self._ent_tables[entity_type],
+                               'fecha_inicio': self._ent_dates[entity_type]}
+        return self._do_the_macroquery(subquery, self._ent_dates[entity_type])
 
     def _codes_to_categories(self, codes):
         roles = []
@@ -176,29 +180,24 @@ class SigidiConnection:
         else:
             return self.can_view_all_convenios()
 
-    def _get_entity_where_has_permission(self, entity, entity_type,
-                                         permission=None):
+    def _get_entity_where_has_permission(self, entity, entity_type):
         """
         Gets the specified project if the user has permission. If permission
         is not specified returns project where user has any permission
         """
 
-        date_row = sigidi_dates[entity_type]
+        date_row = self._ent_dates[entity_type]
 
         if self._can_view_all_entities(entity_type):
-            subquery = self.one_entity_query % {
-                'entity': entity, 'table': sigidi_tables[entity_type],
+            subquery = Sqry.one_entity_query % {
+                'entity': entity, 'table': self._ent_tables[entity_type],
                 'fecha_inicio': date_row}
             entities = self._do_the_macroquery(subquery, date_row)
             if entities:
                 entity = entities[0]
                 return entity
-
-        if permission is None:
-            permission = [SigidiPermissions.CONTAB_LIST,
-                          SigidiPermissions.CONTAB_RES]
-        else:
-            permission = [permission]
+        permission = [SigidiPermissions.CONTAB_LIST,
+                      SigidiPermissions.CONTAB_RES]
 
         # We ask to database for the projects that the user
         # has permissions for
@@ -211,10 +210,12 @@ class SigidiConnection:
         return False
 
     def _get_entities_where_is_ip(self, entity_type):
-        date_row = sigidi_dates[entity_type]
-        subquery = self.entities_where_is_ip_query.format(
-            self.user.profile.documento) % {'table': sigidi_tables[entity_type],
-                                            'fecha_inicio': date_row}
+        date_row = self._ent_dates[entity_type]
+
+        subquery = Sqry.entities_where_is_ip_query.format(
+            self.user.profile.documento
+        ) % {'table': self._ent_tables[entity_type], 'fecha_inicio': date_row}
+
         entities = self._do_the_macroquery(subquery, date_row)
         return entities
 
@@ -224,11 +225,11 @@ class SigidiConnection:
             if entity_ip['CODIGO'] == entity_id:
                 return entity_ip
         entity_perm = self._get_entity_where_has_permission(entity_id,
-                                                             entity_type)
+                                                            entity_type)
         return entity_perm
 
     def _do_the_macroquery(self, subquery, date_format):
-        return self._make_query_dict(self.macroquery % {
+        return self._make_query_dict(Sqry.macroquery % {
             'entities_query': subquery,
             'fecha_inicio': date_format})
 
@@ -238,37 +239,31 @@ class SigidiConnection:
     def get_convenio(self, convenio_id):
         return self._get_entity(convenio_id, SigidiEntityType.CONVENIOS)
 
-    def can_contab_res(self, project):
-        return bool(self.get_project(project, SigidiPermissions.CONTAB_RES))
-
-    def can_contab_list(self, project):
-        return bool(self.get_project(project, SigidiPermissions.CONTAB_LIST))
-
     def get_user_roles(self):
-        roles = self._make_query(self.user_categories_query.format(
+        roles = self._make_query(Sqry.user_categories_query.format(
             self.user.username))
         return set(self._codes_to_categories(roles))
 
     def can_view_all_projects(self):
-        return self._has_any_role(self.proyectos_permissions)
+        return self._has_any_role(self._proyectos_permissions)
 
     def can_view_all_convenios(self):
-        return self._has_any_role(self.convenios_permissions)
+        return self._has_any_role(self._convenios_permissions)
 
     def get_all_projects(self):
         if self.can_view_all_projects():
-            date_row = sigidi_dates[SigidiEntityType.PROYECTOS]
-            entities_query = self.all_entities_query % {
-                'table': sigidi_tables[SigidiEntityType.PROYECTOS],
+            date_row = self._ent_dates[SigidiEntityType.PROYECTOS]
+            entities_query = Sqry.all_entities_query % {
+                'table': self._ent_tables[SigidiEntityType.PROYECTOS],
                 'fecha_inicio': date_row}
             return self._do_the_macroquery(entities_query, date_row)
         return None
 
     def get_all_convenios(self):
         if self.can_view_all_convenios():
-            date_row = sigidi_dates[SigidiEntityType.CONVENIOS]
-            entities_query = self.all_entities_query % {
-                'table': sigidi_tables[SigidiEntityType.CONVENIOS],
+            date_row = self._ent_dates[SigidiEntityType.CONVENIOS]
+            entities_query = Sqry.all_entities_query % {
+                'table': self._ent_tables[SigidiEntityType.CONVENIOS],
                 'fecha_inicio': date_row}
             return self._do_the_macroquery(entities_query, date_row)
         return None
