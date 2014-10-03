30d73048e7de2619b916c88d84138926111b0a86
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-10-03 10:53:17 +0100

    Deleted logger unused

--- statistics/management/commands/calc_stats.py
+++ statistics/management/commands/calc_stats.py
@@ -6,9 +6,6 @@ from django.core.management.base import BaseCommand, CommandError
 from optparse import make_option
 from statistics.models import Department, ProfessionalCategory
 from django.conf import settings as st
-import logging
-
-logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
