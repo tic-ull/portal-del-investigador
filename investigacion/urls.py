fb1e1bd77d2001f800e5e9c5e9837bdf1a66efcf
dyeray <dyeray@ull.es>
2014-10-07 14:20:42 +0100

    Restored urls.py and test_selenium to add again spanish language urls to both files

--- investigacion/urls.py
+++ investigacion/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from flatpages_i18n.models import FlatPage_i18n
 from django.views.generic import TemplateView, RedirectView
 from django.views.generic.detail import DetailView
 
@@ -19,13 +18,16 @@ urlpatterns = patterns(
     url(r'^investigacion/accounts/logout/$',
         'django_cas.views.logout', name='logout'),
     url(r'^investigacion/tinymce/', include('tinymce.urls')),
+)
+
+urlpatterns += i18n_patterns(
+    '',
+    url(r'^investigacion/$', RedirectView.as_view(
+        url=st.BASE_URL), name='index'),
     url(r'^investigacion/cvn/', include('cvn.urls')),
     url(r'^investigacion/estadisticas/', include('statistics.urls')),
     url(r'^investigacion/faq/$', TemplateView.as_view(
         template_name='core/faq/faq.html'), name='faq'),
-    url(r'^investigacion/faq/(?P<pk>\d+)/$', DetailView.as_view(
-        template_name='core/faq/question_faq.html', model=FlatPage_i18n),
-        name='question_faq'),
     url(r'^investigacion/contabilidad/', include('accounting.urls')),
 )
 
