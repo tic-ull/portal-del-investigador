2732ab0315a1adce30c71fda74d72cbc4df5aa3c
dyeray <dyeray@ull.es>
2014-12-05 13:24:19 +0000

    Move some production parsers from cvn managers to parsers/read

--- cvn/parsers/read.py
+++ cvn/parsers/read.py
@@ -304,6 +304,7 @@ def _parse_dedication_type(node):
 
 
 def _parse_cvnitem_profession(node):
+    """Shared parser for current and old profession"""
     date = parse_date_interval(node.find('Date'))
     item = {'title': node.find('Title/Name/Item').text,
             'start_date': date[0],
@@ -319,6 +320,71 @@ def _parse_cvnitem_profession(node):
     item['department'] = (entities[st_cvn.FC_ENTITY.DEPT.value]
                           if entities[st_cvn.FC_ENTITY.DEPT.value] is not None
                           else entities[st_cvn.FC_ENTITY.CURRENT_DEPT.value])
+    return item
+
+
+def _parse_cvnitem_scientificexp(node):
+    """Shared parser for project and agreement"""
+    date_node = node.find('Date')
+    date = parse_date_interval(date_node)
+
+    item = {'titulo': parse_title(node),
+            'fecha_de_inicio': date[0],
+            'fecha_de_fin': date[1],
+            'duracion': date[2],
+            'autores': parse_authors(node.findall('Author'))}
+
+    item.update(parse_economic(node.findall('EconomicDimension')))
+    if node.find('ExternalPK/Code'):
+        item[u'cod_segun_financiadora'] = unicode(node.find(
+            'ExternalPK/Code/Item').text.strip())
+    item.update(parse_scope(node.find('Scope')))
+    return item
+
+
+def parse_cvnitem_scientificexp_project(node):
+    return _parse_cvnitem_scientificexp(node)
+
+
+def parse_cvnitem_scientificexp_agreement(node):
+    return _parse_cvnitem_scientificexp(node)
+
+
+def parse_cvnitem_teaching_phd(node):
+    entities = parse_entities(node.findall('Entity'))
+    item = {'titulo': parse_title(node),
+            'autor': parse_authors(node.findall('Author')),
+            'codirector': parse_authors(node.findall('Link/Author')),
+            'fecha': parse_date(node.find('Date')),
+            'universidad_que_titula': entities[
+                st_cvn.FC_ENTITY.PHD_UNIVERSITY.value]}
+    return item
+
+
+def parse_cvnitem_scientificexp_property(node):
+
+    places = parse_places(node.findall("Place"))
+    entities = parse_entities(node.findall("Entity"))
+    num_solicitud = parse_produccion_id(node.findall('ExternalPK'),
+                                        st_cvn.PRODUCCION_ID_CODE['SOLICITUD'])
+
+    item = {'titulo': parse_title(node),
+            'num_solicitud': num_solicitud,
+            'lugar_prioritario': places[0],
+            'lugares': places[1],
+            'autores': parse_authors(node.findall('Author')),
+            'entidad_titular': entities[st_cvn.FC_ENTITY.OWNER.value],
+            'empresas': entities[st_cvn.FC_ENTITY.OPERATOR.value]}
+
+    dates = node.findall('Date')
+    for date in dates:                              # There can be 2 dates
+        parsed_date = parse_date(date)
+        date_type = date.find("Moment/Item").text
+        if date_type == st_cvn.REGULAR_DATE_CODE:   # Date of request
+            item['fecha'] = parsed_date
+        else:                                       # And date of granting
+            item['fecha_concesion'] = parsed_date
+
     return item
 
 
