228a532d3df1d61d2bf50f25132d3a45572fc585
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-12-12 10:45:20 +0000

    Admin Command for Organize CVN files to new path

--- cvn/helpers.py
+++ cvn/helpers.py
@@ -4,11 +4,11 @@ import hashlib
 import os
 
 
-def get_root_path(self):
+def get_md5_path(self):
     md5 = hashlib.md5(self.user_profile.documento).hexdigest()[0:2]
     return "cvn/%s/" % md5
 
 
 def get_cvn_path(self, filename):
     return os.path.join(
-        get_root_path(self), self.user_profile.documento, filename)
+        get_md5_path(self), self.user_profile.documento, filename)
--- cvn/management/commands/organize_cvn_files.py
+++ cvn/management/commands/organize_cvn_files.py
@@ -5,6 +5,7 @@ from cvn.models import CVN
 from django.conf import settings as st
 from django.core.files.move import file_move_safe
 from django.core.management.base import BaseCommand
+from cvn.helpers import get_cvn_path
 
 import os
 
@@ -15,26 +16,29 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for cvn in CVN.objects.all():
             try:
-                pdf_name = u'%s/CVN-%s.pdf' % (
-                    st_cvn.PDF_ROOT, cvn.user_profile.documento)
-                new_pdf_path = os.path.join(
-                    st.MEDIA_ROOT, pdf_name)
+                filename = u'CVN-%s.pdf' % cvn.user_profile.documento
+                pdf_path = get_cvn_path(cvn, filename)
+                new_pdf_path = os.path.join(st.MEDIA_ROOT, pdf_path)
+
+                filename = u'CVN-%s.xml' % cvn.user_profile.documento
+                xml_path = get_cvn_path(cvn, filename)
+                new_xml_path = os.path.join(st.MEDIA_ROOT, xml_path)
+
+                root_path = '/'.join(new_pdf_path.split('/')[:-1])
+                if not os.path.isdir(root_path):
+                    os.makedirs(root_path)
+
                 if cvn.cvn_file.path != new_pdf_path:
                     file_move_safe(
                         cvn.cvn_file.path, new_pdf_path, allow_overwrite=True)
-                    cvn.cvn_file.name = pdf_name
+                    cvn.cvn_file.name = pdf_path
 
-                xml_name = u'%s/CVN-%s.xml' % (
-                    st_cvn.XML_ROOT, cvn.user_profile.documento)
-                new_xml_path = os.path.join(
-                    st.MEDIA_ROOT, xml_name)
                 if cvn.xml_file.path != new_xml_path:
                     file_move_safe(
                         cvn.xml_file.path, new_xml_path, allow_overwrite=True)
-                    cvn.xml_file.name = xml_name
+                    cvn.xml_file.name = xml_path
 
                 cvn.save()
-
             except Exception as e:
                 print 'User: %s - CVN: %s' % (
                     cvn.user_profile.user, cvn.cvn_file)
