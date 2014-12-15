4f649d91ed1e7f8fe640723eab82f6e23f2b4723
dyeray <dyeray@ull.es>
2014-12-15 11:20:46 +0000

    Logging of status cvn moved to signal

--- cvn/signals.py
+++ cvn/signals.py
@@ -3,6 +3,9 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models.signals import pre_save, pre_delete
 from models import Proyecto, Convenio, CVN
+from core import settings as st_core
+from cvn import settings as st_cvn
+from core.models import Log
 import datetime
 
 
@@ -32,3 +35,26 @@ def cvn_delete_files(sender, instance, **kwargs):
     instance.xml_file.delete(False)
 
 pre_delete.connect(cvn_delete_files, sender=CVN)
+
+
+def log_status_cvn_changed(sender, instance, **kwargs):
+    updated = False
+    try:
+        db_instance = instance.__class__.objects.get(pk=instance.pk)
+    except ObjectDoesNotExist:
+        updated = True
+    else:
+        if instance.status != db_instance.status:
+            updated = True
+
+    if updated:
+        Log.objects.create(
+            user_profile=instance.user_profile,
+            application=instance._meta.app_label.upper(),
+            entry_type=st_core.LogType.CVN_STATUS,
+            date=datetime.datetime.now(),
+            message=st_cvn.CVN_STATUS[instance.status][1]
+        )
+
+pre_save.connect(log_status_cvn_changed, sender=CVN,
+                 dispatch_uid='log_status_cvn_changed')
