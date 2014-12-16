a0d271d8a17b39a559cac88033a02efd52909c3d
Luis Cerrudo <lcerrudo@ull.edu.es>
2014-12-16 11:40:28 +0000

    Refactor admincommand assign_cvn_historical

--- cvn/management/commands/assign_cvn_historical.py
+++ cvn/management/commands/assign_cvn_historical.py
@@ -16,6 +16,8 @@ import os
 
 class Command(BaseCommand):
     help = u'Assigna al usuario los antiguos CVNs como historicos '
+    is_documento = lambda self, username: username[1:-1].isdigit() and \
+                                          username[-1].isalpha()
 
     def get_user(self, search_dict={}):
         try:
@@ -28,20 +30,21 @@ class Command(BaseCommand):
         filename = 'old/CVN-%s' % user.profile.documento
         for data in cvn.split('-')[2:]:
             filename += '-%s' % data
-        file_path = os.path.join(st_cvn.OLD_PDF_ROOT, cvn)
+        old_file_path = os.path.join(st_cvn.OLD_PDF_ROOT, cvn)
+        file_path = os.path.join(st_cvn.OLD_PDF_ROOT, filename.split('/')[-1])
+        os.rename(old_file_path, file_path)
+        # The object is created to calculate the new path
         old_cvn = OldCvnPdf(user_profile=user.profile,
-                            cvn_file=SimpleUploadedFile(
-                                cvn,
-                                open(file_path).read(),
-                                content_type=st_cvn.PDF),
+                            cvn_file=None,
                             uploaded_at=datetime.datetime.now())
-        old_cvn.save()
+        # Relocate old cvn
         old_cvn_path = get_cvn_path(old_cvn, filename)
         old_cvn_new_path = os.path.join(st.MEDIA_ROOT, old_cvn_path)
         root_dir = '/'.join(old_cvn_new_path.split('/')[:-1])
         if not os.path.isdir(root_dir):
             os.makedirs(root_dir)
         file_move_safe(file_path, old_cvn_new_path, allow_overwrite=True)
+        # The resulting file from the object creation is deleted
         old_cvn.cvn_file.name = old_cvn_path
         old_cvn.save()
 
@@ -52,12 +55,12 @@ class Command(BaseCommand):
             search_dict = {'username': username}
             user = self.get_user(search_dict)
             # Search for DNI in "username" field
-            if not user and username[:-1].isdigit() and username[-1].isalpha():
-                search_dict['username'] = username[:-1]
+            if not user and self.is_documento(username):
+                search_dict['username__icontains'] = username[:-1]
                 user = self.get_user(search_dict)
-            # Search  "document" field in the table "profile"
-            if not user:
-                user = self.get_user({'profile__documento__iexact': username})
+                # Search  "document" field in the table "profile"
+                if not user :
+                    user = self.get_user({'profile__documento__iexact': username})
             if user:
                 self.cvn2user(user, cvn)
             else:
