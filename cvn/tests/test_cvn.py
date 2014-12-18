e047cc2075796027974a222ea71ad4a9a7c92b27
Rayco Abad Martín <rabadmar@ull.edu.es>
2014-12-18 14:02:00 +0000

    Refactoring Test CVN

--- cvn/tests/test_cvn.py
+++ cvn/tests/test_cvn.py
@@ -25,21 +25,22 @@ class CVNTestCase(TestCase):
                              'xml/CVN-Test.xml'))
 
     def test_on_insert_cvn_old_pdf_is_moved(self):
-        us = UserFactory.create()
-        cvn = CVN(user=us, pdf_path=os.path.join(
+        user = UserFactory.create()
+        cvn = CVN(user=user, pdf_path=os.path.join(
             st_cvn.TEST_ROOT, 'cvn/CVN-Test.pdf'))
         cvn.save()
-        relative_path = (
-            'old/' + cvn.cvn_file.name.split('/')[-1].split('.')[0] + '-' +
-            cvn.uploaded_at.strftime('%Y-%m-%d-%Hh%Mm%Ss') + '.pdf')
-        root_dir = '/'.join(cvn.cvn_file.path.split('/')[:-1])
-        full_path = os.path.join(root_dir, relative_path)
-        CVN(user=us, pdf_path=os.path.join(
+        filename = cvn.cvn_file.name.split('/')[-1].replace(
+            u'.pdf', u'-' + str(
+                cvn.uploaded_at.strftime('%Y-%m-%d-%Hh%Mm%Ss')
+            ) + u'.pdf')
+        old_cvn_path = os.path.join(
+            '/'.join(cvn.cvn_file.path.split('/')[:-1]), 'old', filename)
+        CVN(user=user, pdf_path=os.path.join(
             st_cvn.TEST_ROOT, 'cvn/CVN-Test.pdf'))
-        self.assertTrue(os.path.isfile(full_path))
-        self.assertEqual(
-            OldCvnPdf.objects.filter(user_profile=us.profile,
-                                     uploaded_at=cvn.uploaded_at).count(), 1)
+        import pdb; pdb.set_trace()
+        self.assertTrue(os.path.isfile(old_cvn_path))
+        self.assertEqual(OldCvnPdf.objects.filter(
+            user_profile=user.profile, uploaded_at=cvn.uploaded_at).count(), 1)
 
     def test_valid_identity_nif_without_letter(self):
         user = UserFactory.create()
