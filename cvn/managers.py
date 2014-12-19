45ae1a14dd6f714bff3bd3255ddb6b017ceb7ff3
dyeray <dyeray@ull.es>
2014-12-19 12:15:37 +0000

    Removed duplicates control from the insertion of productions

--- cvn/managers.py
+++ cvn/managers.py
@@ -9,32 +9,12 @@ import datetime
 
 class CvnItemManager(models.Manager):
 
-    search_items = ['titulo']
-
     def create(self, item, user_profile):
-        insertion_dict = parse_cvnitem(item)
-        search_dict = self._get_search_dict(insertion_dict)
-        if not len(search_dict):
-            reg = self.model(**insertion_dict)
-        else:
-            try:
-                reg = self.model.objects.get(**search_dict)
-            except ObjectDoesNotExist:
-                reg = self.model(**insertion_dict)
-            else:
-                for key, value in insertion_dict.items():
-                    setattr(reg, key, value)
+        item = parse_cvnitem(item)
+        reg = self.model(**item)
         reg.save()
         reg.user_profile.add(user_profile)
         return reg
-
-    # Creates a slice of dictionary with keys used to search
-    def _get_search_dict(self, dictionary):
-        out_dict = {}
-        for item in self.search_items:
-            if item in dictionary and dictionary[item] is not None:
-                out_dict[item + '__iexact'] = dictionary[item]
-        return out_dict
 
     def byUsuariosYear(self, usuarios, year):
         return self.model.objects.filter(
