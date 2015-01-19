ccbd3e267d3d40878dd68d0065055f3412103d09
Rayco Abad Martín <rabadmar@ull.edu.es>
2015-01-19 09:36:29 +0000

    PEP8

--- cvn/helpers.py
+++ cvn/helpers.py
@@ -6,21 +6,21 @@ import os
 
 
 # Do not touch. Uses self so it can be called in a django upload_to field
-# https://stackoverflow.com/questions/17539720/django-model-with-filefield-dynamic-upload-to-argument
+# https://stackoverflow.com/questions/17539720/
 def get_md5_path(self):
     md5 = hashlib.md5(self.user_profile.documento).hexdigest()[0:2]
     return "cvn/%s/" % md5
 
 
 # Do not touch. Uses self so it can be called in a django upload_to field
-# https://stackoverflow.com/questions/17539720/django-model-with-filefield-dynamic-upload-to-argument
+# https://stackoverflow.com/questions/17539720/
 def get_cvn_path(self, filename):
     return os.path.join(
         get_md5_path(self), self.user_profile.documento, filename)
 
 
 # Do not touch. Uses self so it can be called in a django upload_to field
-# https://stackoverflow.com/questions/17539720/django-model-with-filefield-dynamic-upload-to-argument
+# https://stackoverflow.com/questions/17539720/
 def get_old_cvn_path(self, filename):
     return os.path.join(
         get_md5_path(self), self.user_profile.documento, 'old', filename)
