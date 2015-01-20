1e4459cc698f9acbf1428fc744704338dea03206
dyeray <dyeray@ull.es>
2015-01-20 12:57:55 +0000

    Removed split code

--- accounting/settings_local-sample.py
+++
@@ -1,9 +0,0 @@
-# -*- encoding: UTF-8 -*-
-
-ACCOUNTING_SETTINGS_LOCAL = True
-from .settings import *
-
-SIGIDI_DB['NAME'] = 'name'
-SIGIDI_DB['USER'] = 'user'
-SIGIDI_DB['PASSWORD'] = 'password'
-SIGIDI_DB['HOST'] = 'localhost'
