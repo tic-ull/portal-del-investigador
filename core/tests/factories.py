da5f3f7eda1c97fc061136d579fbe75619ed4ee7
Rayco Abad Martín <rabadmar@ull.edu.es>
2015-01-22 12:18:55 +0000

    Updated license

--- core/tests/factories.py
+++ core/tests/factories.py
@@ -1,22 +1,25 @@
 # -*- encoding: UTF-8 -*-
 
 #
-#    Copyright 2014-2015 STIC-Investigación - Universidad de La Laguna (ULL) <gesinv@ull.edu.es>
+#    Copyright 2014-2015
+#
+#      STIC-Investigación - Universidad de La Laguna (ULL) <gesinv@ull.edu.es>
 #
 #    This file is part of Portal del Investigador.
 #
-#    Portal del Investigador is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    Portal del Investigador is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    Portal del Investigador is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with Portal del Investigador.  If not, see <http://www.gnu.org/licenses/>.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with Portal del Investigador.  If not, see
+#    <http://www.gnu.org/licenses/>.
 #
 
 from core.models import UserProfile
