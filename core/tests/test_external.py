83d7df8f75dbaf003ebec415c8f7db76c8349238
Rayco Abad Martín <rabadmar@ull.edu.es>
2015-01-22 11:51:48 +0000

    Added license AGPL

--- core/tests/test_external.py
+++ core/tests/test_external.py
@@ -1,5 +1,27 @@
 # -*- encoding: UTF-8 -*-
 
+#
+#    Copyright 2014-2015 STIC-Investigación - Universidad de La Laguna (ULL)
+#    <gesinv@ull.edu.es>
+#
+#    This file is part of Portal del Investigador.
+#
+#    Portal del Investigador is free software: you can redistribute it and/or
+#    modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Portal del Investigador is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Portal del Investigador.  If not, see
+#    <http://www.gnu.org/licenses/>.
+#
+
 from django.test import TestCase
 from django.conf import settings as st
 import urllib
