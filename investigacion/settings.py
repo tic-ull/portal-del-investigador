394dde85a8da9b454fb8ef24d7d60152467e5c44
Rayco Abad Martín <rabadmar@ull.edu.es>
2015-03-03 13:23:40 +0000

    Deleted JQUERY URL

--- investigacion/settings.py
+++ investigacion/settings.py
@@ -42,7 +42,6 @@ MEDIA_URL = '/investigacion/media/'
 MEDIA_TEST_URL = '/media_tests/'
 LOGIN_URL = 'login'  # Login address for login_required decorator
 TINYMCE_JS_URL = os.path.join(STATIC_URL, 'js/tinymce/resources/tinymce.min.js')
-JQUERY_JS_URL = os.path.join(STATIC_URL, 'js/jquery/resources/jquery.min.js')
 DATATABLES_JS_URL = os.path.join(STATIC_URL, 'js/jquery_datatables/resources/media/js/jquery.dataTables.min.js')
 DATATABLES_CSS_URL = os.path.join(STATIC_URL, 'js/jquery_datatables/resources/media/css/jquery.dataTables.min.css')
 TINYMCE_JS_TEXTAREA = os.path.join(STATIC_URL, 'js/tinymce/conf/textarea.js')
