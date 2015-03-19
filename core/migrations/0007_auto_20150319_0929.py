0ccad3f7530786db969c1691631b9dd17b77ba00
dyeray <dyeray@ull.es>
2015-03-19 11:01:05 +0000

    Added a permission to access the admin menu

---
+++ core/migrations/0007_auto_20150319_0929.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.contrib.contenttypes.management import update_all_contenttypes
+
+
+def add_staff_menu_permission(apps, schema_editor):
+    update_all_contenttypes()  # Fixes tests
+    ContentType = apps.get_model('contenttypes.ContentType')
+    Permission = apps.get_model('auth.Permission')
+    content_type = ContentType.objects.get(app_label='auth', model='user')
+    Permission.objects.create(content_type=content_type,
+                              codename='view_admin_menu',
+                              name='Can view Admin Menu')
+
+
+def delete_staff_menu_permission(apps, schema_editor):
+    apps.get_model('auth.Permission').objects.get(
+        codename='view_admin_menu').delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0006_auto_20150317_1249'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_staff_menu_permission,
+                             delete_staff_menu_permission),
+    ]
