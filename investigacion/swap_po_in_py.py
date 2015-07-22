a7700063a968078ca98b71b8e9c039533578560b
Hugo Jiménez <hjimeneh@ull.edu.es>
2015-07-22 14:18:34 +0100

    Translation fix OMG!; All Submodules updated

--- investigacion/swap_po_in_py.py
+++ investigacion/swap_po_in_py.py
@@ -38,49 +38,59 @@ class XCHG_translations:
 		self.root_path = path
 
 	def replace(self,filename, lineno, esp_str,eng_str):
+
 		#Create temp file
+		if filename.endswith(".html.py"):
+			filename = splitext(filename)[0]
 		print("Fichero:%s " % (filename))
 		src_file_path = self.get_path(filename)
 		fh, dst_path = mkstemp()
 		with io.open(src_file_path, 'r',encoding='utf-8') as src_file:
-			code= src_file.readlines()
+			code = src_file.readlines()
 		
 
 		if self.get_type(filename) == u'.html':
 			import ipdb; ipdb.set_trace()			
 			#import ipdb; ipdb.set_trace()
 			pattern = u'{% trans '+ u'"'+esp_str+u'"' +u' %}'
+			eng_pattern = u'{% trans '+ u'"'+eng_str+u'"' +u' %}'
 			try:
 						
 				if pattern not in code[lineno-1]:
-					pattern = u'{% trans '+ '\''+esp_str+'\'' +u' %}'				
-				code[lineno-1] = code[lineno-1].replace(pattern,'{% trans '+'"'+eng_str+'"'+' %}')			
-				with io.open(dst_path,'w',encoding='utf-8') as dst_file:
-					dst_file.writelines(code)		
-				#Remove original file
-				remove(src_file_path)
-				#Move new file
-				move(dst_path, src_file_path)
+					pattern = u'{% trans '+ '\''+esp_str+'\'' +u' %}'
+					eng_pattern = u'{% trans '+ u'\''+eng_str+u'\'' +u' %}'
+				if eng_pattern not in code[lineno-1]:
+					code[lineno-1] = code[lineno-1].replace(pattern,'{% trans '+'"'+eng_str+'"'+' %}')
+					with io.open(dst_path,'w',encoding='utf-8') as dst_file:
+						dst_file.writelines(code)
+					#Remove original file
+					remove(src_file_path)
+					#Move new file
+					move(dst_path, src_file_path)
 			except UnicodeDecodeError:
 				print (UnicodeDecodeError)	
 		if 	self.get_type(filename) == u'.py':
 			#import ipdb; ipdb.set_trace()
 			pattern = u'_(' + '"' + esp_str + '"' + ')'
+			eng_pattern = u'_(' + '"' + eng_str + '"' + ')'
 			try:
 				if pattern not in code[lineno-1]:							
 					pattern = u'_(u' + '"' + esp_str + '"' + ')'
+					eng_pattern = u'_(u' + '"' + eng_str + '"' + ')'
 				if pattern not in code[lineno-1]:
 					pattern = u'_(u' + '\'' + esp_str + '\'' + ')'
+					eng_pattern = u'_(u' + '\'' + eng_str + '\'' + ')'
 				if pattern not in code[lineno-1]:													
 					pattern = u'_(' + '\'' + esp_str + '\'' + ')'
-
-				code[lineno-1] = code[lineno-1].replace(pattern,u'_('+'"'+eng_str+'"'+')')			
-				with io.open(dst_path,'w',encoding='utf-8') as dst_file:
-					dst_file.writelines(code)		
-				#Remove original file
-				remove(src_file_path)
-				#Move new file
-				move(dst_path, src_file_path)
+					eng_pattern = u'_(' + '\'' + eng_str + '\'' + ')'
+				if eng_pattern not in code[lineno-1]:
+					code[lineno-1] = code[lineno-1].replace(pattern,u'_('+'"'+eng_str+'"'+')')
+					with io.open(dst_path,'w',encoding='utf-8') as dst_file:
+						dst_file.writelines(code)
+					#Remove original file
+					remove(src_file_path)
+					#Move new file
+					move(dst_path, src_file_path)
 			except UnicodeDecodeError:
 				print (UnicodeDecodeError)	
 import sys
@@ -92,14 +102,14 @@ except ImportError:
   print 'Download it at https://pypi.python.org/pypi/polib/'
   sys.exit(1)
 
-if len(sys.argv) != 2:
-  print 'USAGE: %s <PO file>' % sys.argv[0]
+if len(sys.argv) != 3:
+  print 'USAGE: %s <PO file> <ROOT DIR>' % sys.argv[0]
   sys.exit(1)
 
 filename = sys.argv[1]
 
 XCHG=XCHG_translations()
-XCHG.set_root_path('/home/hugo/Programs/core')
+XCHG.set_root_path(sys.argv[2])
 XCHG.load_po(filename)
 
 # Swap every unit, except the first one,
@@ -110,11 +120,12 @@ for entry in valid_entries:
 	esp = entry.msgid
 	eng = entry.msgstr
 	for occ in entry.occurrences:
-		if u'.html.py' not  in occ[0]:				
-			try:
-	
-					XCHG.replace(occ[0],int(occ[1]),esp,eng)
-			except IOError:
-				print (IOError)
-				pass
-		
+		try:
+				print (occ)
+				XCHG.replace(occ[0],int(occ[1]),esp,eng)
+		except IOError:
+			print (IOError)
+			pass
+
+
+
