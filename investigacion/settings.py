cc6cdd1673dcc4758d3a0c21fbea52765e5b8e79
Hugo Jiménez <hjimeneh@ull.edu.es>
2015-10-21 14:08:01 +0100

    Refactored HISTORICAL dictionary

--- investigacion/settings.py
+++ investigacion/settings.py
@@ -176,10 +176,6 @@ DATABASES = {
     }
 }
 
-HISTORICAL = {
-    'year': 'historica',
-}
-
 DATABASE_ROUTERS = ['core.routers.DynamicDbRouter']
 
 # ******************************* DATABASES *********************************
@@ -272,7 +268,7 @@ except NameError:
     except ImportError:
         pass
 # ************************* SETTINGS LOCAL ***********************************
-
+HISTORICAL = [d for d in DATABASES if d != 'default']
 # ******************************* LOGGING ************************************
 LOG_FILENAME = os.path.join(LOG_ROOT, 'investigacion.log')
 LOGGING = {
